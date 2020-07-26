066fdd921a24c8ab7cec594fca6535b44e7b11ab
kiskis <fakekis@yahoo.com>
2020-07-26 16:56:00 +0200

    removed login req for topics page

--- blogs/views.py
+++ blogs/views.py
@@ -10,12 +10,14 @@ def index(request):
     """The home page for Blog"""
     return render(request,'blogs/index.html')
 
-@login_required
 def blog_posts(request):
     """Show all blog posts."""
-    blogposts1 = BlogPost.objects.filter(owner=request.user)
-    blogposts2 = BlogPost.objects.filter(public=True)
-    blog_posts = blogposts1.union(blogposts2).order_by('date_added')
+    if request.user.is_authenticated():
+        blogposts1 = BlogPost.objects.filter(owner=request.user)
+        blogposts2 = BlogPost.objects.filter(public=True)
+        blog_posts = blogposts1.union(blogposts2).order_by('date_added')
+    else: 
+         blog_posts = BlogPost.objects.filter(public=True)
     context = {'blog_posts':blog_posts}
     return render(request, 'blogs/blog_posts.html',context)
 
